ebdc40756c501ede3b01dfd0bfbeb0c172b947fb
MarsIfeanyi <marschalice@gmail.com>
2023-10-08 23:00:43 +0100

    Create Mint and Burn functions

--- src/MarsIfeanyi.sol
+++ src/MarsIfeanyi.sol
@@ -8,4 +8,12 @@ contract MarsIfeanyi is ERC20 {
     constructor() ERC20("Mars Ifeanyi Receipt Token", "MIRT") {
         _mint(msg.sender, 1_0000_000_000e18);
     }
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
+    }
 }
