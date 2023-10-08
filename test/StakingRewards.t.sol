5cd476379fe1d353cefc429f9333b46c0f0faa27
MarsIfeanyi <marschalice@gmail.com>
2023-10-08 20:35:49 +0100

    Update test setup

--- test/StakingRewards.t.sol
+++ test/StakingRewards.t.sol
@@ -9,8 +9,10 @@ contract StakingRewrdsTest is Test {
     StakingRewrds public stakingRewrds;
     MarsIfeanyi public marsIfeanyi;
 
+    address public WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
     function setUp() public {
-        stakingRewrds = new StakingRewrds();
+        stakingRewrds = new StakingRewrds(address(marsIfeanyi), WETH);
 
         marsIfeanyi = new MarsIfeanyi();
     }
